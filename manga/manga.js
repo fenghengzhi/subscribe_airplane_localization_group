e410a6a692f0fd728730c2a8499118c0a28a2711
fenghengzhi <fenghengzhi@users.noreply.github.com>
2017-05-28 16:37:43 +0800

    Update manga.js

--- manga/manga.js
+++ manga/manga.js
@@ -7,27 +7,27 @@
 		$('.mangabox').remove();//移除所有漫画
 		$('#chapterbox').hide();
 		var mangaboxtamplate=$('.mangaboxtamplate');
-		for(var url in mangadata){
+		for(var mangaurl in mangadata){
 			var mangabox=mangaboxtamplate.clone(true).addClass('mangabox');
 			try{
 				//console.log('start');
 				mangabox.show();
 				mangabox.appendTo(mangalistbox);
-				mangabox.attr('mangaurl',url);
-				mangabox.attr('time',mangadata[url].time);
-				mangabox.attr('name',mangadata[url].name);
-				mangabox[0].time=mangadata[url].time;
-				mangabox[0].name=mangadata[url].name;
+				mangabox[0].mangaurl=mangaurl;
+				mangabox[0].time=mangadata[mangaurl].time;
+				mangabox[0].name=mangadata[mangaurl].name;
 				//mangabox.find('.shwochapterbutton')[0].chapterdata=mangadata[url].chapterdata;
-				mangabox.find('.manganame').text(mangadata[url].name||url);
-				mangabox.find('.mangacover').attr('coverurl',mangadata[url].coverurl);
-				mangabox.find('.lastview').attr('url',url);
-				mangabox.find('.lastview').attr('mangaurl',url);
-				mangabox.find('.lastview').attr('chapternum',mangadata[url].lastview);
-				mangabox.find('.lastviewtitle').text('看到：'+(mangadata[url].chapterdata.chapters[(mangadata[url].lastview)]||{title:"未观看"}).title);
-				mangabox.find('.newest').attr('url',mangadata[url].chapterdata.chapters[mangadata[url].chapterdata.chapters.length-1].url);
-				mangabox.find('.newest').attr('mangaurl',url);
-				mangabox.find('.newest').attr('chapternum',mangadata[url].chapterdata.chapters.length-1);
+				mangabox.find('.manganame').text(mangadata[mangaurl].name||mangaurl);
+				mangabox.find('.mangacover')[0].coverurl=mangadata[mangaurl].coverurl;
+				var lastview=mangabox.find('.lastview')[0];
+				lastview.url=mangadata[mangaurl].chapterdata.chapters[(mangadata[mangaurl].lastview)].url;
+				lastview.mangaurl=mangaurl;
+				lastview.chapternum'=mangadata[mangaurl].lastview;
+				mangabox.find('.lastviewtitle').text('看到：'+(mangadata[mangaurl].chapterdata.chapters[(mangadata[mangaurl].lastview)]||{title:"未观看"}).title);
+				var newest=mangabox.find('.newest')[0];
+				newest.url=mangadata[mangaurl].chapterdata.chapters[mangadata[mangaurl].chapterdata.chapters.length-1].url;
+				newest.mangaurl=mangaurl;
+				newest.chapternum=mangadata[mangaurl].chapterdata.chapters.length-1;
 				mangabox.find('.newesttitle').text('最新：'+(mangadata[url].chapterdata.chapters[mangadata[url].chapterdata.chapters.length-1]||{title:"未更新"}).title);
 				//console.log('end');
 			}finally{continue;}
@@ -55,18 +55,18 @@
 	//alert();
 	$.get("mangalist.txt", function(data, textStatus){ 
 		var mangadatastore=JSON.parse(window.localStorage.mangadata||'{}');
-		var mangalist=data.trim().split('\r\n');
+		var mangalist=data.trim().replace(/\r\n/g,'\n').replace(/\r/g,'\n').split('\n');
 		//console.log(mangalist);
 		var mangacount=mangalist.length;
 		waitupdate.mangacount=mangacount;
 		//console.log(mangadatastore);
-		mangalist.forEach(function(url){
+		mangalist.forEach(function(mangaurl){
 			//console.log(url,mangadatastore[url]);
-			mangadata[url]=mangadatastore[url]||{
+			mangadata[mangaurl]=mangadatastore[mangaurl]||{
 				chapterdata:{
 					author:undefined,
 					brief:undefined,
-					url:url,
+					mangaurl:mangaurl,
 					chapters:[{title:undefined,url:undefined}]
 				},
 				coverurl:undefined,
@@ -82,42 +82,42 @@
 		//console.log('updatenum in checkupdate',waitupdate.updatecount);
 		waitupdate.updatecount=0;
 		//console.log('updatenum in checkupdate',waitupdate.updatecount);
-		for(var url in mangadata){
-			update(url);
+		for(var mangaurl in mangadata){
+			(function(mangaurl){
+				$.get("/proxy?url="+encodeURIComponent(mangaurl), function(data, textStatus){ 
+					if(textStatus==='success'){
+						var manga=$(data.replace(/src=/g,'_src='));
+						//manga.find("a:contains('最新章节')[class='am-btn am-btn-primary am-radius']");
+						mangadata[mangaurl].coverurl=manga.find('.am-img-thumbnail').attr('_src');
+						mangadata[mangaurl].name=manga.filter('title').text();
+						mangadata[mangaurl].chapterdata=mangadata[mangaurl].chapterdata||{};
+						mangadata[mangaurl].chapterdata.author=manga.find('div.am-u-sm-8 > abbr:nth-child(3)').text();
+						mangadata[mangaurl].chapterdata.brief=manga.find('div.am-u-sm-8 > div').text();
+						mangadata[mangaurl].chapterdata.mangaurl=mangaurl;
+						var chapters=manga.find('a.am-btn-secondary');
+						if(mangadata[mangaurl].chapterdata.chapters.length!==chapters.length){
+							mangadata[mangaurl].time=(new Date()).valueOf();//时间戳精确到毫秒
+							var chapters=manga.find('a.am-btn-secondary');
+							mangadata[mangaurl].chapterdata.chapters=new Array(chapters.length);//集数信息
+							//mangadata[url].chapters.forEach()
+							for(var i=0;i<chapters.length;++i){
+								mangadata[mangaurl].chapterdata.chapters[i]={
+									url:chapters[i].href,
+									title:chapters[i].text
+								};
+							}
+						}
+						localStorage.mangadata=JSON.stringify(mangadata);
+					}
+					waitupdate();
+				});
+			})(mangaurl);
 		}
 		event.stopPropagation();
 		event.preventDefault();
 	}
 	$("#updatebutton").one('click',checkupdate);
-	function update(url){
-		$.get("/proxy?url="+encodeURIComponent(url), function(data, textStatus){ 
-			if(textStatus==='success'){
-				var manga=$(data.replace(/src=/g,'_src='));
-				//manga.find("a:contains('最新章节')[class='am-btn am-btn-primary am-radius']");
-				mangadata[url].coverurl=manga.find('.am-img-thumbnail').attr('_src');
-				mangadata[url].name=manga.filter('title').text();
-				mangadata[url].chapterdata=mangadata[url].chapterdata||{};
-				mangadata[url].chapterdata.author=manga.find('div.am-u-sm-8 > abbr:nth-child(3)').text();
-				mangadata[url].chapterdata.brief=manga.find('div.am-u-sm-8 > div').text();
-				mangadata[url].chapterdata.url=url;
-				var chapters=manga.find('a.am-btn-secondary');
-				if(mangadata[url].chapterdata.chapters.length!==chapters.length){
-					mangadata[url].time=(new Date()).valueOf();//时间戳精确到毫秒
-					var chapters=manga.find('a.am-btn-secondary');
-					mangadata[url].chapterdata.chapters=new Array(chapters.length);//集数信息
-					//mangadata[url].chapters.forEach()
-					for(var i=0;i<chapters.length;++i){
-						mangadata[url].chapterdata.chapters[i]={
-							url:chapters[i].href,
-							title:chapters[i].text
-						};
-					}
-				}
-				localStorage.mangadata=JSON.stringify(mangadata);
-			}
-			waitupdate();
-		});
-	}
+
 // $('body').append('<button>测试按钮</button>').click(function(){
 // window.open("http://smp.yoedge.com/view/omnibus/1001178");
 // });
